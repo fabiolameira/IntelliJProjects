f127930ea345cbc02de1b0435952c0d975ca04cb
Fábio Lameira <36962813+fabiolameira97@users.noreply.github.com>
2018-05-18 11:29:00 +0100

    Initial Commit

---
+++ MultiThreadingExample_I/src/exemplo_I/TestTread.java
@@ -0,0 +1,14 @@
+package exemplo_I;
+
+public class TestTread {
+
+    public static void main(String[] args) {
+        
+        RunnableDemo R1 = new RunnableDemo("Thread#1");
+        R1.start();
+
+        RunnableDemo R2 = new RunnableDemo("Thread#2");
+        R1.start();
+        
+    }
+}
---
+++ MultiThreadingExample_II/src/exemplo_II/ThreadMain.java
@@ -0,0 +1,14 @@
+package exemplo_II;
+
+public class ThreadMain {
+
+    public static void main(String[] args) {
+
+        CountUp CU = new CountUp("CountUp");
+        CU.start();
+
+        CountDown CD = new CountDown("CountDown");
+        CD.start();
+
+    }
+}
